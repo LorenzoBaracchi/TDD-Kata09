f95623a03e052f7c70c509c58beee702e68f99a8
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2014-12-05 12:37:58 +0100

    refactor compute discount

--- supermarket.rb
+++ supermarket.rb
@@ -8,10 +8,7 @@ def goodPrice(good)
 end
 
 def discount(goods)
-  if goods.count('A') >= 3
-    then return 20 * (goods.count('A')/3).to_i
-  end
-  return 0
+  return 20 * (goods.count('A')/3).to_i
 end
 
 def price(goods)
