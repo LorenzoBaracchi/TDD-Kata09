1cd5a813f9b2e929664ba70b1088aa9086587685
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2014-12-09 15:52:11 +0100

    refactor checkout

--- supermarket.rb
+++ supermarket.rb
@@ -6,19 +6,26 @@ RULES = {:goods=> Goods, :discounts => Discounts}
 class CheckOut
 
   def initialize(pricing_rules)
-    @total = 0
     @items = []
     @goodsRules = pricing_rules[:goods]
     @discountsRules = pricing_rules[:discounts]
   end
 
   def scan(item)
-  @total += @goodsRules.priceFor(item)
-  @items << item
+    @items << item
   end
 
   def total
-    @total - @discountsRules.discount(@items)
+    computeTotal - @discountsRules.discount(@items)
+  end
+
+  private
+  def computeTotal
+    total = 0
+    @items.each do |item|
+      total += @goodsRules.priceFor(item)
+    end
+    total
   end
 end
 
