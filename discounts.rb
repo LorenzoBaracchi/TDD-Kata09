eb6943c8e0bc578a3baca6d2d740231a2536d590
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2014-12-09 16:21:10 +0100

    refactor discounts

--- discounts.rb
+++ discounts.rb
@@ -1,23 +1,35 @@
 module Discounts
-  @@prices = Hash.new
-  @@prices['A'] = 20
-  @@prices['B'] = 15
 
-  @@quantity = Hash.new
-  @@quantity['A'] = 3
-  @@quantity['B'] = 2
+  class Discount
 
-  def self.discount(goods)
-    discountFor(goods, 'A') + discountFor(goods, 'B')
+    attr_accessor :amount
+    attr_accessor :quantity
+
+    def initialize(amount, quantity)
+      @amount = amount
+      @quantity = quantity
+    end
+  end
+
+  @@rules = Hash.new
+  @@rules['A'] = Discount.new(20, 3)
+  @@rules['B'] = Discount.new(15, 2)
+
+  def self.discount(goodsList)
+    total = 0
+    @@rules.each do |key, pair|
+      total += discountFor(goodsList, key, pair)
+    end
+    total
   end
 
   private
-  def self.discountFor(goods, discountedGood)
-    @@prices[discountedGood] * numberOfDiscountedItems(goods, discountedGood)
+  def self.discountFor(goodsList, good, discount)
+    discount.amount * numberOfDiscountedItems(goodsList, good, discount)
   end
 
-  def self.numberOfDiscountedItems(goods, discountedGood)
-    (goods.count(discountedGood)/@@quantity[discountedGood]).to_i
+  def self.numberOfDiscountedItems(goodsList, good, discount)
+    (goodsList.count(good)/discount.quantity).to_i
   end
 
 end
